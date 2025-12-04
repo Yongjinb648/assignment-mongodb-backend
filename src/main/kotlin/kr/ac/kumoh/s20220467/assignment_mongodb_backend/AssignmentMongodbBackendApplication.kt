c768449903b896997e95231727c58aee26ff8bdd
Sharpa1215 <hifriend333@naver.com>
2025-12-04 21:55:37 +0900

    🎉Project 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/AssignmentMongodbBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220467.assignment_mongodb_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssignmentMongodbBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssignmentMongodbBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/AssignmentMongodbBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220467.assignment_mongodb_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentMongodbBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
