abe4039f99f018087868c5f3258bb1b8b8ac4ace
Sharpa1215 <hifriend333@naver.com>
2025-12-05 15:17:13 +0900

    🔧URL 속성 추가(차후 사용)

--- src/main/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/controller/GuitarController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/controller/GuitarController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/guitars")
-@CrossOrigin(origins = ["http://localhost:5174"])
+@CrossOrigin(origins = ["http://localhost:5173"])
 class GuitarController(
     private val service: GuitarService
 ) {
--- src/main/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/model/Guitar.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220467/assignment_mongodb_backend/model/Guitar.kt
@@ -12,4 +12,5 @@ data class Guitar(
     val bridge: String,
     val color: String,
     val price: Int,
+    var imageUrl: String,
 )
